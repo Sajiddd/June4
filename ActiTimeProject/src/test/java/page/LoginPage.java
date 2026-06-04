db36830fe275cdfec88c9b409012bd148a8232cd
Asus <Asus@Asus-TUF-A17>
2026-06-04 17:33:11 +0530

    updated

--- ActiTimeProject/src/test/java/page/LoginPage.java
+++ ActiTimeProject/src/test/java/page/LoginPage.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LoginPage 
 {
@@ -20,6 +22,10 @@ public class LoginPage
 	//login button web element
 	@FindBy(xpath = "//div[text()='Login ']")
 	private WebElement loginButton;
+	
+	//error msg
+	@FindBy(xpath = "//span[text()='Username or Password is invalid. Please try again.']")
+	private WebElement errMessage;
 	
 	//initializing non static variables using constructor
 	public LoginPage(WebDriver driver)
@@ -47,7 +53,19 @@ public class LoginPage
 		loginButton.click();
 	}
 	
-	
+	//verify error msg is displayed
+	public boolean verifyErrorMessageIsDisplayed(WebDriverWait wait)
+	{
+		try
+		{
+			wait.until(ExpectedConditions.visibilityOf(errMessage));
+			return true;
+		}
+		catch (Exception e) {
+			
+		}
+		return false;
+	}
 	
 	
 	
